29e72ab92251330670bede6f9c3f0c9281673b21
Suhani31Agrawal <73304019+Suhani31Agrawal@users.noreply.github.com>
2020-11-20 21:50:13 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,20 @@
+class Bob {
+    constructor(x,y,r) {
+      var options = {
+          friction:0.8,
+          density:1.2
+      }
+      this.body = Bodies.circle(x,y,r,options);
+      this.r = r;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x,pos.y)
+      ellipseMode(CENTER);
+      fill("yellow");
+      ellipse(0, 0, this.r*2, this.r*2);
+      pop();
+    }
+  };
